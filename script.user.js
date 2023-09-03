07b6873d4266761fab97f5ce7b5ecd29c523288e
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2023-09-03 12:06:38 +0900

    version

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Ads Sckiper
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       hirotada-t
 // @match        https://www.youtube.com/watch*
