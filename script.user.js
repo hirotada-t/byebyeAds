b715d476bdbc49284738fde58b394032b8d08671
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2023-09-03 12:27:03 +0900

    debug

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Ads Sckiper
 // @namespace    http://tampermonkey.net/
-// @version      0.1.3
+// @version      0.1.4
 // @description  try to take over the world!
 // @author       hirotada-t
 // @match        https://www.youtube.com/watch*
@@ -20,8 +20,9 @@ setInterval(() => {
   // const skipBtnList = document.querySelectorAll(".ytp-ad-skip-button");
   // const existSkip = exist(skipBtnList);
   if (existAds) {
-    moviePlayer.playbackRate = 16;
-    moviePlayer.muted = true;
+    const video = moviePlayer.querySelector("video");
+    video.playbackRate = 16;
+    video.muted = true;
   }
   // if (existSkip) {
   //   skipBtnList.forEach((b) => {
